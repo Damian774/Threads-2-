664b6d7596c453c7c68bcc03ad3d954c783fc5fc
Damian-PWSZ <CrimsonShade774@gmail.com>
2018-10-26 14:31:57 +0200

    init

---
+++ app/src/main/java/com/example/pwsz/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.pwsz.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+jeden z elementow programowych wykonywany na proc
+
+watek to proces bez wlasnej pamieci - ma pamiec wspoldzielona
